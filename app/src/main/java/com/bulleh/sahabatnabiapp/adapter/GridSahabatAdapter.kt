d9f85868168c18ca8de86b43245ab5d6ce45959e
febriansya <darmansyahyan21@gmail.com>
2021-10-08 22:15:07 +0800

    tes

---
+++ app/src/main/java/com/bulleh/sahabatnabiapp/adapter/GridSahabatAdapter.kt
@@ -0,0 +1,24 @@
+package com.bulleh.sahabatnabiapp.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GridSahabatAdapter : RecyclerView.Adapter<GridSahabatAdapter.SahabatViewHolder>() {
+
+    inner class SahabatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SahabatViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SahabatViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/bulleh/sahabatnabiapp/detail/DetailSahabatActivity.kt
@@ -0,0 +1,12 @@
+package com.bulleh.sahabatnabiapp.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bulleh.sahabatnabiapp.R
+
+class DetailSahabatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_sahabat)
+    }
+}
